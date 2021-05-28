3dd1230e8f573f9dfe460ca23abe71c18c4f4a7c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-05-28 19:03:44 +0200

    Fix: typo
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- controls/resource_group.rb
+++ controls/resource_group.rb
@@ -12,20 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-resource_group = attribute('resourcegroup', description:'Azure resource group')
-
 portefaix_version = input('portefaix_version')
-portefaix_section = 'vnet'
+portefaix_section = 'resource_group'
 
-title "VNet standards"
+title "Resource Group standards"
 
-# VNET.1
+# RG.1
 # =======================================================
 
 portefaix_req = "#{portefaix_section}.1"
 
 control "portefaix-azure-#{portefaix_version}-#{portefaix_req}" do
-  title "Ensure that virtual networks have tags"
+  title "Ensure that resource groups have tags"
   impact 1.0
 
   tag standard: "portefaix"
@@ -35,9 +33,8 @@ control "portefaix-azure-#{portefaix_version}-#{portefaix_req}" do
   
   ref "Portefaix Azure #{portefaix_version}, #{portefaix_section}"
 
-  describe azure_virtual_networks(resource_group: resource_group) do
+  describe azure_resource_groups do
     it { should exist }
-    its('tags') { should include(service: 'vpc') }
     its('tags') { should include("made-by": 'terraform') }
   end
 
--- controls/vnet.rb
+++ controls/vnet.rb
@@ -12,20 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# resource_group = attribute('resourcegroup', description:'Azure resource group')
+resource_group = attribute('resourcegroup', description:'Azure resource group')
 
 portefaix_version = input('portefaix_version')
-portefaix_section = 'resource_group'
+portefaix_section = 'vnet'
 
-title "Resource Group standards"
+title "VNet standards"
 
-# RG.1
+# VNET.1
 # =======================================================
 
 portefaix_req = "#{portefaix_section}.1"
 
 control "portefaix-azure-#{portefaix_version}-#{portefaix_req}" do
-  title "Ensure that resource groups have tags"
+  title "Ensure that virtual networks have tags"
   impact 1.0
 
   tag standard: "portefaix"
@@ -35,8 +35,9 @@ control "portefaix-azure-#{portefaix_version}-#{portefaix_req}" do
   
   ref "Portefaix Azure #{portefaix_version}, #{portefaix_section}"
 
-  describe azure_resource_groups do
+  describe azure_virtual_networks(resource_group: resource_group) do
     it { should exist }
+    its('tags') { should include(service: 'vpc') }
     its('tags') { should include("made-by": 'terraform') }
   end
 
